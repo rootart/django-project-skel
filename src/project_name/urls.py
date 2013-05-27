afdf622f228f11ac677764b7ecd0b15c943bbfa5
rootart <dijakroot@gmail.com>
2013-05-27 19:18:06 +0300

    Add debug 404/500 pages by using TemplateView

--- src/project_name/urls.py
+++ src/project_name/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf import settings
+from django.views.generic import TemplateView
 
 admin.autodiscover()
 
@@ -16,4 +17,8 @@ if settings.DEBUG:
         url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {
             'document_root': settings.STATIC_ROOT,
         }),
+    )
+    urlpatterns += patterns('',
+        url(r'^error-page/404/$', TemplateView.as_view(template_name='404.html')),
+        url(r'^error-page/500/$', TemplateView.as_view(template_name='500.html'))
     )
